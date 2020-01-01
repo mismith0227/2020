7df644247e85a09738290e3211f126d69fb040b4
mismith0227 <doraepon2216@gmail.com>
2020-01-01 11:12:07 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const config = require('./config')
+const { src, dest, watch } = require('gulp')
+const del = require('del')
+const sass = require('gulp-sass')
+const plumber = require('gulp-plumber')
+const webpackStream = require('webpack-stream')
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config')
+
+// HTML
+// =====================================================
+const html = () => {
+  return src(`${config.tasks.html.src}`).pipe(
+    gulp.dest(`${config.tasks.html.dest}`)
+  )
+}
+
+// SCSS
+// =====================================================
+const compileSass = () => {
+  return src(config.tasks.scss.src)
+    .pipe(
+      sass({
+        outputStyle: config.envProduction ? 'compressed' : 'nested'
+      }).on('error', sass.logError)
+    )
+    .pipe(dest(config.tasks.scss.dest))
+}
+
+// Images
+// =====================================================
+const minifyImages = () => {
+  return src(config.tasks.images.src).pipe(dest(config.tasks.images.dest))
+}
+
+// webpack
+// =====================================================
+const compileJavascript = () => {
+  return src(config.tasks.webpack.src)
+    .pipe(plumber())
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(dest(config.tasks.webpack.dest))
+}
+
+// Font
+// =====================================================
+const font = () => {
+  return src(config.tasks.fonts.src).pipe(dest(config.tasks.fonts.dest))
+}
+
+// Clean
+// =====================================================
+const clean = cb => {
+  return del(config.tasks.clean, cb)
+}
+
+// Server
+// =====================================================
+const server = done => {
+  browserSync.init(config.tasks.server.browserSyncOptions)
+  done()
+  console.log('Server was launched')
+}
+
+// Watch
+// =====================================================
+const watchFiles = done => {
+  watch(config.tasks.html.src, html).on('change', browserSync.reload)
+  watch(config.tasks.scss.src, compileSass).on('change', browserSync.reload)
+  watch(config.tasks.webpack.src, compileJavascript).on(
+    'change',
+    browserSync.reload
+  )
+  watch(config.tasks.images.src, minifyImages).on('change', browserSync.reload)
+  watch(config.tasks.fonts.src, font).on('change', browserSync.reload)
+  done()
+}
+
+// Default
+// =====================================================
+const defaultTask = gulp.series(
+  html,
+  compileSass,
+  compileJavascript,
+  minifyImages,
+  font,
+  watchFiles,
+  server
+)
+
+// Build
+// =====================================================
+const build = gulp.series(
+  clean,
+  html,
+  compileSass,
+  compileJavascript,
+  minifyImages,
+  font
+)
+
+exports.build = build
+
+exports.default = defaultTask
